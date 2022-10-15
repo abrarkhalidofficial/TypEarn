7e40be6731955b4e6582c20e32c34954f5bf9d65
M Abrar Khalid <abrarprince471@gmail.com>
2022-10-15 12:53:55 +0500

    TeamSection Some Fixes

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
 import team from "../assets/team.png";
+import Mehfooz from "../assets/mehfooz-ul-rehman.jpeg";
+import Muzamil from "../assets/muzamil.jpeg";
+import MD from "../assets/md-asfar.png";
+import Hammad from "../assets/hammad-habib.jpeg";
+import Abdullah from "../assets/m-abdullah.jpeg";
+import Ameer from "../assets/ammer.jpeg";
+
 import { Linkedin } from "react-feather";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -29,15 +36,20 @@ export function Teams() {
           <SwiperSlide>
             <div className="teams__content__entry">
               <img
-                src={team}
+                src={Abdullah}
                 alt="team"
                 className="teams__content__entry__img"
               />
               <div className="teams__content__entry__heading">
-                ABDULLAh ShAhiD
+                M. Abdullah Shahid
               </div>
-              <div className="teams__content__entry__info">DEVELOPER</div>
-              <a href="#" className="teams__content__entry__icon">
+              <div className="teams__content__entry__info">
+                Blockchain Developer
+              </div>
+              <a
+                href="https://www.linkedin.com/in/muhammad-abdullah-s-0781a394/"
+                className="teams__content__entry__icon"
+              >
                 <Linkedin size={20} color="currentColor" />
               </a>
             </div>
@@ -45,15 +57,20 @@ export function Teams() {
           <SwiperSlide>
             <div className="teams__content__entry">
               <img
-                src={team}
+                src={Mehfooz}
                 alt="team"
                 className="teams__content__entry__img"
               />
               <div className="teams__content__entry__heading">
-                ABDULLAh ShAhiD
+                Mehfooz-ur-Rehman
               </div>
-              <div className="teams__content__entry__info">DEVELOPER</div>
-              <a href="#" className="teams__content__entry__icon">
+              <div className="teams__content__entry__info">
+                Full Stack Developer
+              </div>
+              <a
+                href="https://www.linkedin.com/in/mehfooz-rehman-37a1b0231/"
+                className="teams__content__entry__icon"
+              >
                 <Linkedin size={20} color="currentColor" />
               </a>
             </div>
@@ -61,15 +78,18 @@ export function Teams() {
           <SwiperSlide>
             <div className="teams__content__entry">
               <img
-                src={team}
+                src={Hammad}
                 alt="team"
                 className="teams__content__entry__img"
               />
-              <div className="teams__content__entry__heading">
-                ABDULLAh ShAhiD
+              <div className="teams__content__entry__heading">Hammad Habib</div>
+              <div className="teams__content__entry__info">
+                UI/UX Design Lead
               </div>
-              <div className="teams__content__entry__info">DEVELOPER</div>
-              <a href="#" className="teams__content__entry__icon">
+              <a
+                href="https://luna-starter.web.app/www.linkedin.com/in/hammadhabib"
+                className="teams__content__entry__icon"
+              >
                 <Linkedin size={20} color="currentColor" />
               </a>
             </div>
@@ -77,15 +97,31 @@ export function Teams() {
           <SwiperSlide>
             <div className="teams__content__entry">
               <img
-                src={team}
+                src={Muzamil}
                 alt="team"
                 className="teams__content__entry__img"
               />
               <div className="teams__content__entry__heading">
-                ABDULLAh ShAhiD
+                Muzammil Haider
               </div>
-              <div className="teams__content__entry__info">DEVELOPER</div>
-              <a href="#" className="teams__content__entry__icon">
+              <div className="teams__content__entry__info">CEO</div>
+              <a
+                href="https://www.linkedin.com/in/muzammil-haider-1a8526148"
+                className="teams__content__entry__icon"
+              >
+                <Linkedin size={20} color="currentColor" />
+              </a>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="teams__content__entry">
+              <img src={MD} alt="team" className="teams__content__entry__img" />
+              <div className="teams__content__entry__heading">Md Asfar</div>
+              <div className="teams__content__entry__info">Marketing Lead</div>
+              <a
+                href="https://www.linkedin.com/in/md-afsar/"
+                className="teams__content__entry__icon"
+              >
                 <Linkedin size={20} color="currentColor" />
               </a>
             </div>
@@ -93,15 +129,18 @@ export function Teams() {
           <SwiperSlide>
             <div className="teams__content__entry">
               <img
-                src={team}
+                src={Ameer}
                 alt="team"
                 className="teams__content__entry__img"
               />
               <div className="teams__content__entry__heading">
-                ABDULLAh ShAhiD
+                Ameer A. Khammas
               </div>
-              <div className="teams__content__entry__info">DEVELOPER</div>
-              <a href="#" className="teams__content__entry__icon">
+              <div className="teams__content__entry__info">Marketing Lead</div>
+              <a
+                href="https://luna-starter.web.app/"
+                className="teams__content__entry__icon"
+              >
                 <Linkedin size={20} color="currentColor" />
               </a>
             </div>
